4ce7f09e8582751d51c22ea4302eef772265f06f
Ganesh Varadarajan <ganesh@coriolis.co.in>
2023-10-19 20:56:23 +0530

    added basic layout with URL and buttons

--- SMSReaderApp/app/src/main/java/com/example/smsreaderapp/MainActivity.kt
+++ SMSReaderApp/app/src/main/java/com/example/smsreaderapp/MainActivity.kt
@@ -52,6 +52,12 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import android.util.Log
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : Activity() {
 
@@ -59,15 +65,18 @@ class MainActivity : Activity() {
         const val SMS_PERMISSION_CODE = 123
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    // Declare Views
+    private lateinit var activityLogRecyclerView: RecyclerView
+    private lateinit var urlEditText: EditText
+    private lateinit var confirmUrlButton: Button
+    private lateinit var startButton: Button
+    private lateinit var stopButton: Button
+    private lateinit var resetButton: Button
+    private lateinit var sheetStatusTextView: TextView
+    private lateinit var forwardingStatusTextView: TextView
 
-        // Check for SMS permission
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECEIVE_SMS), SMS_PERMISSION_CODE)
-        }
-    }
+    // List to store activity logs
+    private val activityLogs = mutableListOf<String>()
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         Log.d("SmsReceiverPermission", "fooo")  // Log the SMS detail
@@ -81,4 +90,78 @@ class MainActivity : Activity() {
             }
         }
     }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Check for SMS permission
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECEIVE_SMS), SMS_PERMISSION_CODE)
+        }
+
+        // Bind Views
+        activityLogRecyclerView = findViewById(R.id.activityLogRecyclerView)
+        urlEditText = findViewById(R.id.urlEditText)
+        confirmUrlButton = findViewById(R.id.confirmUrlButton)
+        startButton = findViewById(R.id.startButton)
+        stopButton = findViewById(R.id.stopButton)
+        resetButton = findViewById(R.id.resetButton)
+        sheetStatusTextView = findViewById(R.id.sheetStatusTextView)
+        forwardingStatusTextView = findViewById(R.id.forwardingStatusTextView)
+
+        // Set up RecyclerView
+        activityLogRecyclerView.layoutManager = LinearLayoutManager(this)
+        val adapter = LogAdapter(activityLogs)
+        activityLogRecyclerView.adapter = adapter
+
+        // Set up listeners
+        confirmUrlButton.setOnClickListener {
+            val url = urlEditText.text.toString()
+            // Validate and set the URL for your Google Sheet (implementation depends on your logic)
+            logActivity("URL set to $url")
+        }
+
+        startButton.setOnClickListener {
+            // Start forwarding logic here
+            logActivity("Started forwarding")
+            forwardingStatusTextView.text = "Forwarding Status: In progress"
+        }
+
+        stopButton.setOnClickListener {
+            // Stop forwarding logic here
+            logActivity("Stopped forwarding")
+            forwardingStatusTextView.text = "Forwarding Status: Stopped"
+        }
+
+        resetButton.setOnClickListener {
+            // Reset logic here
+            logActivity("Reset performed")
+        }
+    }
+
+    private fun logActivity(log: String) {
+        if (activityLogs.size >= 10) {
+            activityLogs.removeAt(0)  // remove the oldest log
+        }
+        activityLogs.add(log)
+        activityLogRecyclerView.adapter?.notifyDataSetChanged()
+        activityLogRecyclerView.smoothScrollToPosition(activityLogs.size - 1) // scroll to the latest log
+    }
+
+}
+
+// Adapter for the RecyclerView
+class LogAdapter(private val logs: List<String>) : RecyclerView.Adapter<LogAdapter.ViewHolder>() {
+    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val textView = TextView(parent.context)
+        return ViewHolder(textView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = logs[position]
+    }
+
+    override fun getItemCount() = logs.size
 }
